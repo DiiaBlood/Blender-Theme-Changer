02ce1b6c9ba76c72c770f2fa32616d2613559d43
MuktadaCreations <MuktadaCreations@gmail.com>
2021-04-03 14:57:00 +0300

    April [03] C1
    - Some color changes to the UI
    - Auto detect Blender Folder on windows
    -New color adjustment mathod

--- Blender_Theme_Changer.py
+++ Blender_Theme_Changer.py
@@ -1,6 +1,9 @@
 from xml.dom import minidom
 import tkinter as tk
 from tkinter import DISABLED, NORMAL, HORIZONTAL, DoubleVar, colorchooser, filedialog
+from colour import Color
+from getpass import getuser
+from os import path
 
 
 
@@ -23,7 +26,7 @@ InterfaceNodesWithoutAlpha = [1, 2, 17]
 NodesWithShading = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19]
 
 
-def DarkenColor(hex_color, brightness_offset=1):
+def DarkenColor(hex_color, brightness_offset):
     """ takes a color like #87c95f and produces a lighter or darker variant """
     if len(hex_color) != 7:
         raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
@@ -36,8 +39,18 @@ def DarkenColor(hex_color, brightness_offset=1):
 
 def pickfile():
     # Showing a File dialog for loading the prefrances blend file
-    filepath = filedialog.askopenfilename(initialdir="/", title="Theme Preset File",
-    filetypes=(("Preset File", "*.xml"), ("all files", "*.*")))
+
+    # Checks to see if the easiest way to the Blender Config exists
+    WinEasyPath = "C:\\Users\\" + getuser() + "\\AppData\\Roaming\\Blender Foundation\\Blender\\"
+    
+    # Decides if the EasyPath exist then to start with and if it doesn't then go to the pc home folder
+    if path.exists(WinEasyPath):
+        filepath = filedialog.askopenfilename(initialdir=WinEasyPath, title="Theme Preset File",
+        filetypes=(("Preset File", "*.xml"), ("all files", "*.*")))
+    else:
+        filepath = filedialog.askopenfilename(initialdir="/", title="Theme Preset File",
+        filetypes=(("Preset File", "*.xml"), ("all files", "*.*")))
+
     if filepath != "":
         globals()["BlendFilePath"] = filepath
 
@@ -95,7 +108,9 @@ def Apply():
 
 
     # Making a darker version of the same color for more use
-    DarkSolidColor = str(DarkenColor(BlendColor[1], -150))
+    TEMPDarkSolidColor = DarkenColor(BlendColor[1], -60)
+    TEMPDarkSolidColor2 = Color(BlendColor[1], luminance=0.4)
+    DarkSolidColor = TEMPDarkSolidColor2.hex
     
     
     # HEX Color with alpha value
@@ -155,33 +170,34 @@ def Apply():
 
 
 
+ButtonColor = "#cccccc"
+
 # Background Frame
-BFrame = tk.Frame(root, bg = "black")
+BFrame = tk.Frame(root, bg = "#0f1012")
 BFrame.place(relwidth=1, relheigh=1)
 
 # The Button to pick the userpref blend file
-FileButton = tk.Button(root, text="Preset File", padx=5, pady=5, fg="black", bg="white", command=pickfile)
+FileButton = tk.Button(root, text="Preset File", padx=5, pady=5, fg="#1f1466", bg=ButtonColor, command=pickfile)
 FileButton.place(relwidth=0.9, relheigh=0.2,  relx=0.5, rely=0.15, anchor=tk.CENTER)
 
+# The Button to Make Blender UI Shaded
+ShadeCheckBox = tk.Checkbutton(root, text="Shade", padx=5, pady=5, fg="black", bg=ButtonColor, command=ShadeSwitch)
+ShadeCheckBox.place(relwidth=0.9, relheigh=0.12,  relx=0.5, rely=0.325, anchor=tk.CENTER)
+
 # The Button to choose a color
-ColorPicker = tk.Button(root, text="Color", padx=5, pady=5, fg="black", bg="white", command=pickcolor)
+ColorPicker = tk.Button(root, text="Color", padx=5, pady=5, fg="black", bg=ButtonColor, command=pickcolor)
 ColorPicker.place(relwidth=0.5, relheigh=0.2,  relx=0.3, rely=0.5, anchor=tk.CENTER)
 #The box on the side of the color button showing the color the user choose
 colorframe = tk.Frame(root, bg = '#5680C2')
 colorframe.place(relwidth=0.4, relheigh=0.2, relx=0.75, rely=0.5, anchor=tk.CENTER)
 
-# The Button to Make Blender UI Shaded
-ShadeCheckBox = tk.Checkbutton(root, text="Shade", padx=5, pady=5, fg="black", bg="white", command=ShadeSwitch)
-ShadeCheckBox.place(relwidth=0.9, relheigh=0.12,  relx=0.5, rely=0.325, anchor=tk.CENTER)
-
 # The Value of Blenders UI Roundness
 scale_var = DoubleVar()
 scale_var.set(0.4)
-RoundValueBox = tk.Scale(root, fg="black", bg="white", command=RoundenessValue, orient=HORIZONTAL, from_=0, to=1, resolution=0.01, variable=scale_var)
+RoundValueBox = tk.Scale(root, fg="black", bg=ButtonColor, command=RoundenessValue, orient=HORIZONTAL, from_=0, to=1, resolution=0.01, variable=scale_var)
 RoundValueBox.place(relwidth=0.9, relheigh=0.12,  relx=0.5, rely=0.6725, anchor=tk.CENTER)
 
-
-Apply = tk.Button(root, text="Apply", padx=5, pady=5, fg="black", bg="white", command=Apply, state=DISABLED)
+Apply = tk.Button(root, text="Apply", padx=5, pady=5, fg="black", bg=ButtonColor, command=Apply, state=DISABLED)
 Apply.place(relwidth=0.9, relheigh=0.2,  relx=0.5, rely=0.85, anchor=tk.CENTER)
 
 
